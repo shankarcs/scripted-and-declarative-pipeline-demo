b34deef59dd8655c3ab5dcd7f897d3da8e8adfdc
Prasanjit Singh <mailprasanjit@gmail.com>
2020-05-16 02:39:18 +0400

    Update test.py

--- test.py
+++ test.py
@@ -8,5 +8,13 @@ class TestHello(unittest.TestCase):
         rv = self.app.get('/')
         self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data, b'Hello World!\n')
+
+#Simple test report        
+#if __name__ == '__main__':
+#    unittest.main()
+
+# Jenkins supports to represent the test reports with the JUnit format in the Blue Ocean, so we should change the output of the test like below
 if __name__ == '__main__':
-    unittest.main()
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    unittest.main(testRunner=runner)
