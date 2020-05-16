6e17faa4f97ee96798d7868d4616ec1812104bed
Prasanjit Singh <mailprasanjit@gmail.com>
2020-05-16 12:07:29 +0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hey, we have Python Flask in a Docker container!'
+    return 'Hey, we have Python Flask in a Docker container!\n'
 
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
