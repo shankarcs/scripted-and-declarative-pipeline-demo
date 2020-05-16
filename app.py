65c9f29bf0ccd16c05c712aea771c1cd685b2d96
Prasanjit Singh <mailprasanjit@gmail.com>
2020-05-16 11:59:40 +0400

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ app = Flask(__name__)
 def hello():
     return 'Hey, we have Python Flask in a Docker container!'
 
-if __name == '__main__':
+if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0')
